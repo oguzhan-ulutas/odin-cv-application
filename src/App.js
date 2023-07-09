da6356dbc32eec14b46ef2f5562f4d8559bdaf1b
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-07-09 17:14:38 +0300

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import uniqid from "uniqid";
+import Footer from "./components/footer";
 
 export class App extends Component {
   constructor() {
     super();
     this.state = {
+      id: uniqid(),
       generalInfo: {
-        firstName,
-        lastName,
-        eMail,
-        tel,
+        firstName: "",
+        lastName: "",
+        eMail: "",
+        tel: "",
       },
       education: {
-        institution,
-        dateFrom,
-        dateTo,
-        qalification,
+        institution: "",
+        dateFrom: "",
+        dateTo: "",
+        qalification: "",
       },
       experience: {
-        company,
-        title,
-        dateFrom,
-        dateTo,
-        tasks,
+        company: "",
+        title: "",
+        dateFrom: "",
+        dateTo: "",
+        tasks: "",
       },
     };
   }
   render() {
     return (
       <div className="container">
-        <div className="general-info"></div>
-        <div className="education"></div>
-        <div className="experience"></div>
+        <h1>CV App</h1>
+        <div className="general-info">
+          <h2>General Info</h2>
+          <button>Add</button>
+        </div>
+        <div className="education">
+          <h2>Education</h2>
+          <button>Add</button>
+        </div>
+        <div className="experience">
+          <h2>Experience</h2>
+          <button>Add</button>
+        </div>
+        <Footer />
       </div>
     );
   }
